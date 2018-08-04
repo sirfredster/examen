f7cd4d33a7586384693d2bc42d4930f8f05e322f
sirfred <sirfredster@gmail.com>
2018-08-04 00:21:23 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/controlador/ControladorHolaMundo.java
@@ -0,0 +1,14 @@
+package com.example.demo.controlador;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class ControladorHolaMundo {
+
+    @RequestMapping(value = "/saludar/{nombre}", method = RequestMethod.GET)
+    public @ResponseBody String saludar(@PathVariable("nombre") String nombre) {
+        return "Hola";
+    }
+
+}
